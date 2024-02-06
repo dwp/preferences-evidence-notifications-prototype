f7613c019e73163619efbbd9a261fef0fe9ec962
DavDohDWP <150276629+DavDohDWP@users.noreply.github.com>
2024-02-06 10:53:34 +0000

    Merge branch 'main' of github.com:dwp-ucd/preferences-evidence-notifications
    GitOrigin-RevId: 8c3b21036b28e83c64a082232356a9e3624ea77c

---
+++ app/routes/cxp.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// const verNum = 'v1'
+
+// router.get(`/${verNum}/createUCClaim/account-gather`, (req, res) => {
+//   req.session.gatherTodos = req.session.gatherTodos || gatherData;
+//   let pageData = {
+//     lastCompleted: req.session.lastCompleted,
+//     gatherTodos: req.session.gatherTodos,
+//     allCompleted: req.session.allCompleted,
+//   };
+//   res.render(`${verNum}/createUCClaim/account-gather`, pageData);
+// })
+
+module.exports = router
